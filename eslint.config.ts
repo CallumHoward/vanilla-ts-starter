f6b6d1570159e46c0d19f2feeef600b67d2abb73
CallumHoward <7144420+CallumHoward@users.noreply.github.com>
2025-11-20 12:45:39 +1100

    Add eslint and prettier

---
+++ eslint.config.ts
@@ -0,0 +1,10 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig(
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  prettier,
+);
