bac3da4dd7e7002fe0c62529e67bb18ddf8872b1
CallumHoward <callum.howard@checkbox.ai>
2026-07-02 23:47:51 +1000

    test wip

---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig, configDefaults } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+    exclude: [...configDefaults.exclude],
+  },
+});
